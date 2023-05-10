7748d3b9c194a9bb1fc90e4f9835f76ca8cf8d15
franchesoni <marchesoniacland@gmail.com>
2023-05-10 17:09:33 +0200

    fix get_embeddings

--- predictor.py
+++ predictor.py
@@ -8,11 +8,9 @@ def get_resnet(pretrained=False, n_channels=3, compile=False, device=None):
     assert n_channels in [1, 3]
     net = resnet18(weights=ResNet18_Weights if pretrained else None, num_classes=1)
     if n_channels == 1:
-
         def new_forward(self, x: torch.Tensor) -> torch.Tensor:
             B, C, H, W = x.shape
             return self._forward_impl(x.expand(B, 3, H, W))  # rough conversion to RGB
-
         net.forward = new_forward.__get__(net)
     net.maxpool = torch.nn.Identity()  # to handle sizes correctly
     device = device or (torch.device("cuda" if torch.cuda.is_available() else "cpu"))
@@ -25,7 +23,10 @@ def get_resnet(pretrained=False, n_channels=3, compile=False, device=None):
     return net
 
 
-def get_embedding(predictor, x: torch.Tensor) -> torch.Tensor:
+def compute_embedding(predictor, x: torch.Tensor) -> torch.Tensor:
+    B, C, H, W = x.shape
+    if C == 1:
+        x = x.expand(B, 3, H, W)
     # See note [TorchScript super()]
     x = predictor.conv1(x)
     x = predictor.bn1(x)
@@ -39,6 +40,5 @@ def get_embedding(predictor, x: torch.Tensor) -> torch.Tensor:
 
     x = predictor.avgpool(x)
     embedding = torch.flatten(x, 1)
-    x = predictor.fc(embedding)
-
-    return x, embedding
+    # x = predictor.fc(embedding)  # last layer
+    return embedding
