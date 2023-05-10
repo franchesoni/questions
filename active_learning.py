f7b0ebdccd3676cf728becafaea655dc5e0c43a9
franchesoni <marchesoniacland@gmail.com>
2023-05-10 12:50:07 +0200

    move optimizer creation out of the loop

--- active_learning.py
+++ active_learning.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 import gc
 
-from engine import seed_everything, fit_predictor, evaluate_predictor
+from engine import optimizer_setup, seed_everything, fit_predictor, evaluate_predictor
 from data import DATASET_NAMES, get_dataset, get_labeled_unlabeled
 from predictor import get_resnet
 from visualization import plot_curves
@@ -30,16 +30,20 @@ def get_al_curve(seed, predictor, strategy, train_dataset, test_dataset, initial
   seed_everything(seed)
   curve = [None] * (len(initial_labeled_indices) - 1)
   labeled_indices = initial_labeled_indices
-  labeled_ds, unlabeled_ds = get_labeled_unlabeled(train_dataset, labeled_indices)  # unlabeled_ds also stores the unlabeled indices
-  while True:  # fit, get point, label
-    predictor = fit_predictor(predictor, labeled_ds)
-    performance = evaluate_predictor(predictor, test_dataset)
+  unlabeled_ds = 'a value to start the loop'
+
+  optimizer = optimizer_setup(predictor)
+  loss_fn = torch.nn.BCEWithLogitsLoss()
+  eval_loss_fn = torch.nn.BCEWithLogitsLoss(reduction='sum')
+
+  while len(unlabeled_ds) > 0:  # fit, get point, label
+    labeled_ds, unlabeled_ds = get_labeled_unlabeled(train_dataset, labeled_indices)  # unlabeled_ds also stores the unlabeled indices
+    predictor = fit_predictor(predictor, loss_fn, optimizer, labeled_ds)
+    performance = evaluate_predictor(predictor, eval_loss_fn, test_dataset)
     curve = curve + [performance]  # add performance to curve
     index_to_label_next = strategy(predictor, unlabeled_ds)
     labeled_indices = labeled_indices + [index_to_label_next]
     labeled_ds, unlabeled_ds = get_labeled_unlabeled(train_dataset, labeled_indices)
-    if len(unlabeled_ds) == 0: 
-      break
     gc.collect()
   return curve
 
