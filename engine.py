57284e504c0c836603e03a5ae121071fe79487ae
franchesoni <marchesoniacland@gmail.com>
2023-05-11 01:49:29 +0200

    fit to incorrect

--- engine.py
+++ engine.py
@@ -36,7 +36,7 @@ def fit_predictor(predictor, loss_fn, optimizer, labeled_ds, n_epochs=24, verbos
         print(f"Fitting predictor for {n_epochs} epochs")
     for epoch in range(n_epochs):
         output = predictor(labeled_ds.data)[:, 0]  # remove channel dimension
-        loss = loss_fn(output, labeled_ds.targets)
+        loss = loss_fn(output, labeled_ds.targets)  # outputs should be logits
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -45,6 +45,34 @@ def fit_predictor(predictor, loss_fn, optimizer, labeled_ds, n_epochs=24, verbos
     if verbose:
         print("Done fitting predictor with loss", loss.item(), "       ")
     return predictor
+
+def fit_predictor_with_incorrect(predictor, loss_fn, optimizer, labeled_ds, incorrect_x_y_hat, n_epochs=24, verbose=True):
+    """Here we fit a pytorch model to the labeled dataset. We always allow the model to see visited_datapoints."""
+    # predictor.train()
+
+    if verbose:
+        print(f"Fitting predictor for {n_epochs} epochs")
+    for epoch in range(n_epochs):
+        output = predictor(labeled_ds.data)[:, 0]  # remove channel dimension
+        loss = loss_fn(output, labeled_ds.targets)
+        if incorrect_x_y_hat is not None:
+            x, y_hat = incorrect_x_y_hat
+            pred_y_probs = torch.sigmoid(predictor(x)[:, 0])  # remove channel dimension
+            # y_hat are the labels we guessesd, what we compute here is the probability of those labels being correct (all at the same time)
+            prob_correct = torch.prod(pred_y_probs**y_hat * (1-pred_y_probs)**(1-y_hat))
+            # that probability should be low
+            loss_incorrect_term = torch.nn.functional.binary_cross_entropy(prob_correct, torch.tensor(0.0).to(prob_correct.device))
+            loss += loss_incorrect_term
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if verbose:
+            print(f"Epoch {epoch+1} / {n_epochs}, loss {loss.item()}", end="\r")
+    if verbose:
+        print("Done fitting predictor with loss", loss.item(), "       ")
+    return predictor
+
+
 
 
 def evaluate_predictor(predictor, loss_fn, test_dataset, batch_size=5000, verbose=True):
