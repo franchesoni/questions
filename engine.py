5d19eaafff12d8123c5177f0cda9926db4207e20
franchesoni <marchesoniacland@gmail.com>
2023-05-10 15:14:54 +0200

    remove fitting_to_info, we changed what's info

--- engine.py
+++ engine.py
@@ -42,49 +42,6 @@ def fit_predictor(predictor, loss_fn, optimizer, labeled_ds, n_epochs=24, verbos
     return predictor
 
 
-def fit_predictor_to_info(predictor, info, max_visited_datapoints=60000, max_epochs=3, lr=0.001, optim='adam', batch_size=32, verbose=False):
-    """Here we fit a pytorch model to the labeled dataset. We always allow the model to see visited_datapoints."""
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    predictor = predictor.to(device)
-    predictor = predictor.train()
-    loss_fn = torch.nn.BCEWithLogitsLoss()
-
-    labeled_ds = info['labeled']
-    n_epochs = min(max(max_visited_datapoints // len(labeled_ds), 1), max_epochs)  # always do at least one epoch
-    dl = torch.utils.data.DataLoader(labeled_ds, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=cpu_count(), pin_memory=device=='cuda')  # here comes the stochasticity
-    optimizer = torch.optim.Adam(predictor.parameters(), lr=lr) if optim == 'adam' else torch.optim.SGD(predictor.parameters(), lr=lr)
-
-    if verbose:
-      print(f"Fitting predictor for {n_epochs} epochs")
-
-    times = {'forward': 0, 'backward': 0, 'loss': 0, 'data': 0}
-    for epoch in range(n_epochs):
-        st = time.time()
-        for batch in dl:
-            input, target = batch
-            input, target = input.to(device), target.to(device).float()
-            times['data'] += time.time() - st
-            st = time.time()
-            output = predictor(input)[:, 0]  # select the only output and remove the channel dimension
-            times['forward'] += time.time() - st
-            st = time.time()
-            loss = loss_fn(output, target)
-            times['loss'] += time.time() - st
-            st = time.time()
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            times['backward'] += time.time() - st
-            st = time.time()
-        if verbose:
-            print(f"Epoch {epoch} / {n_epochs}, loss {loss.item()}")
-    if verbose:
-        times = {k: v / n_epochs / len(dl) for k, v in times.items()}
-        print(f"Average times per batch: {times}")
-        print(f"Percentual time per batch: {dict(zip(times.keys(), [v / sum(times.values()) for v in times.values()]))}")
-    return predictor
-
-
         
 def evaluate_predictor(predictor, loss_fn, test_dataset, batch_size=5000, verbose=True):
     """Here we evaluate the predictor on the test dataset."""
