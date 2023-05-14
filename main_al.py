5e96d5f76ad51b35d6758b39d0d43319f0aca339
franchesoni <marchesoniacland@gmail.com>
2023-05-14 22:09:27 +0200

    rename and separate exps

--- main_al.py
+++ main_al.py
@@ -1,15 +1,14 @@
-from active_learning import get_al_curve
+from pathlib import Path
 from engine import seed_everything
 import datetime
 import numpy as np
 
 from data import get_dataset, DATASET_NAMES
 from predictor import get_resnet
-from intelligent_annotation import get_ia_curve
-from active_learning import CoreSet, UncertaintySampling, get_al_curve, RandomSampling
+from active_learning import CoreSet, UncertaintySampling, get_al_curve, RandomSampling, CertaintySampling
 
 
-def run_experiment(
+def run_al_experiment(
     exp_type,
     experiment_name,
     dataset_index,
@@ -21,6 +20,8 @@ def run_experiment(
     use_only_first_test,
     device=None,
 ):
+    assert exp_type == 'al'
+    Path('results/curves').mkdir(parents=True, exist_ok=True)
     dataset_name = DATASET_NAMES[dataset_index]
     train_ds, test_ds = get_dataset(
         dataset_name,
@@ -34,6 +35,8 @@ def run_experiment(
         strategy = RandomSampling()
     elif experiment_name == "uncertainty":
         strategy = UncertaintySampling()
+    elif experiment_name == "certainty":
+        strategy = CertaintySampling()
     elif experiment_name == "coreset":
         strategy = CoreSet()
     for seed in seeds:
@@ -42,49 +45,39 @@ def run_experiment(
         predictor = get_resnet(
             pretrained=pretrained, n_channels=n_channels, compile=False, device=device
         )
-        if exp_type == 'al':
-            curve = get_al_curve(
-                max_queries,
-                predictor,
-                strategy,
-                train_ds,
-                test_ds,
-                initial_labeled_indices=initial_labeled_indices,
-            )   
-        elif exp_type == 'ia':
-            curve = get_ia_curve(
-                max_queries,
-                predictor,
-                strategy,
-                train_ds,
-                test_ds,
-                initial_labeled_indices=initial_labeled_indices,
-            )
+        curve = get_al_curve(
+            max_queries,
+            predictor,
+            strategy,
+            train_ds,
+            test_ds,
+            initial_labeled_indices=initial_labeled_indices,
+        )   
         now = str(datetime.datetime.utcnow()).replace(' ', '-').replace(':', '-').replace('.', '-')
-        dstfilename = f"results/curve_{now}_seed_{seed}_exp_type_{exp_type}_exp_name_{experiment_name}_dataset_{dataset_name}_pretrained_{pretrained}_binarizer_{binarizer}_max_queries_{max_queries}_use_only_first_{use_only_first}_use_only_first_test_{use_only_first_test}.npy"
+        dstfilename = f"results/curves/curve_{now}_seed_{seed}_exp_type_{exp_type}_exp_name_{experiment_name}_dataset_{dataset_name}_pretrained_{pretrained}_binarizer_{binarizer}_max_queries_{max_queries}_use_only_first_{use_only_first}_use_only_first_test_{use_only_first_test}.npy"
         np.save(dstfilename, curve)
 
-def run_all_experiments(dev=False, profiler=None):
+def run_all_experiments(run=0, dev=False, profiler=None):
     for binarizer in ["geq5"]:#, "last", "odd"]:
         for pretrained in [False]:#, True]:  # not working because it needs 1000 classes
             for dataset_index in [1, 0, 2, 3, 4]:
                 for exp_name in ["random", "uncertainty", "certainty"]:#, "coreset"]:  # coreset is broken
                     for exp_type in ['al']:#['ia', 'al']:  # does ia work?
-                        if exp_type == 'ia' and exp_name == 'coreset':
-                            continue  # not implemented
-                        seeds = [0, 1, 2]
-                        device='cuda:0'
-                        # seeds = [3, 4, 5]
-                        # device='cuda:1'
+                        if run==0:
+                            seeds = [0, 1, 2]
+                            device='cuda:0'
+                        if run==1:
+                            seeds = [3, 4, 5]
+                            device='cuda:1'
                         max_queries = 500
-                        use_only_first = 5000
+                        use_only_first = 6000
                         use_only_first_test = 1000
                         if dev:
                             max_queries = 50
                             use_only_first = 500
                             use_only_first_test = 100
                             seeds = [0]
-                        run_experiment(
+                        run_al_experiment(
                             seeds=seeds,
                             exp_type=exp_type,
                             experiment_name=exp_name,
@@ -103,16 +96,12 @@ def run_all_experiments(dev=False, profiler=None):
                             profiler.disable()
                             profiler.dump_stats('run_all_dev.profile')
                             profiler.enable()
-        #                 break
-        #             break
-        #         break
-        #     break
-        # break
+
 
 
 
 if __name__ == "__main__":
-    run_all_experiments(dev=False, profiler=None)
+    run_all_experiments(run=0, dev=False, profiler=None)
     # import cProfile
     # pr = cProfile.Profile()
     # pr.enable()
--- main_ia.py
+++ main_ia.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from active_learning import get_al_curve
 from engine import seed_everything
 import datetime
@@ -10,6 +11,7 @@ from active_learning import CoreSet, UncertaintySampling, get_al_curve, RandomSa
 
 
 def run_ia_experiment(
+    sts_kwargs,
     exp_type,
     experiment_name,
     dataset_index,
@@ -21,6 +23,7 @@ def run_ia_experiment(
     use_only_first_test,
     device=None,
 ):
+    Path('results/curves').mkdir(parents=True, exist_ok=True)
     assert exp_type == 'ia'
     assert experiment_name in ['random', 'uncertainty']
     dataset_name = DATASET_NAMES[dataset_index]
@@ -32,13 +35,7 @@ def run_ia_experiment(
         device=device,
     )
     n_channels = train_ds.data[0].shape[0]
-    sts_kwargs = {
-        "max_expansions": 10,
-        "max_n": 8,
-        "al_method": experiment_name,
-        "cost_fn": "entropy",
-        "reduce_certainty_factor":0.1,
-    }
+
 
     for seed in seeds:
         seed_everything(seed)
@@ -55,55 +52,74 @@ def run_ia_experiment(
             sts_kwargs=sts_kwargs,
             )
         now = str(datetime.datetime.utcnow()).replace(' ', '-').replace(':', '-').replace('.', '-')
-        dstfilename = f"results/curve_{now}_seed_{seed}_exp_type_{exp_type}_exp_name_{experiment_name}_dataset_{dataset_name}_pretrained_{pretrained}_binarizer_{binarizer}_max_queries_{max_queries}_use_only_first_{use_only_first}_use_only_first_test_{use_only_first_test}.npy"
+        dstfilename = f"results/curves/curve_{now}_seed_{seed}_exp_type_{exp_type}_exp_name_{experiment_name}_dataset_{dataset_name}_pretrained_{pretrained}_binarizer_{binarizer}_max_queries_{max_queries}_use_only_first_{use_only_first}_use_only_first_test_{use_only_first_test}.npy"
         np.save(dstfilename, curve)
 
 def run_all_experiments(run=0, dev=False, profiler=None):
     for binarizer in ["geq5"]:
         for pretrained in [False]:#, True]:  # not working because it needs 1000 classes
-            for dataset_index in [1, 0, 2, 3, 4]:
-                for exp_name in ["random", "uncertainty"]:
-                    for exp_type in ['ia']:
-                        max_queries = 500
-                        use_only_first = 5000
-                        use_only_first_test = 1000
-                        if run == 0:
-                            seeds = [0, 1, 2]
-                            device='cuda:0'
-                        elif run == 1:
-                            seeds = [3, 4, 5]
-                            device='cuda:1'
-                        else:
-                            raise ValueError("run must be 0 or 1")
+            for dataset_index in [1, 0, 2, 3]:#, 4]:
+                for exp_name in ["uncertainty", "random"]:
+                    for max_n in [8, 4, 2, 16]:
+                        for cost_fn in ["entropy", "length"]:
+                            for reduce_uncertainty_factor in [0.01, 0.1, 0.5, 0.9]:
+                                sts_kwargs = {
+                                    "max_expansions": 8,
+                                    "max_n": max_n,
+                                    "al_method": exp_name,
+                                    "cost_fn": cost_fn,
+                                    "reduce_certainty_factor":reduce_uncertainty_factor,
+                                }
+                                exp_type = 'ia'
+                                max_queries = 6000
+                                use_only_first = 6000
+                                use_only_first_test = 1000
+                                if run == 0:
+                                    seeds = [0, 1, 2]
+                                    device='cuda:0'
+                                elif run == 1:
+                                    seeds = [3, 4, 5]
+                                    device='cuda:1'
+                                else:
+                                    raise ValueError("run must be 0 or 1")
 
-                        if dev:
-                            max_queries = 50
-                            use_only_first = 500
-                            use_only_first_test = 100
-                            seeds = [0]
+                                if dev:
+                                    max_queries = 5
+                                    use_only_first = 6000
+                                    use_only_first_test = 1000
+                                    seeds = [0]
 
-                        run_ia_experiment(
-                            seeds=seeds,
-                            exp_type=exp_type,
-                            experiment_name=exp_name,
-                            dataset_index=dataset_index,
-                            pretrained=pretrained,
-                            binarizer=binarizer,
-                            max_queries=max_queries,
-                            use_only_first=use_only_first,
-                            use_only_first_test=use_only_first_test,
-                            device=device,
-                        )
-                        print("_"*80)
-                        print(f"Finished {exp_type} {exp_name} {dataset_index} {binarizer} {pretrained}")
-                        print("_"*80)
-                        if profiler and dev:
-                            profiler.disable()
-                            profiler.dump_stats('run_all_dev.profile')
-                            profiler.enable()
+                                run_ia_experiment(
+                                    sts_kwargs=sts_kwargs,
+                                    seeds=seeds,
+                                    exp_type=exp_type,
+                                    experiment_name=exp_name,
+                                    dataset_index=dataset_index,
+                                    pretrained=pretrained,
+                                    binarizer=binarizer,
+                                    max_queries=max_queries,
+                                    use_only_first=use_only_first,
+                                    use_only_first_test=use_only_first_test,
+                                    device=device,
+                                )
+                                print("_"*80)
+                                print(f"Finished {exp_type} {exp_name} {dataset_index} {binarizer} {pretrained}")
+                                print("_"*80)
+                                if profiler and dev:
+                                    profiler.disable()
+                                    profiler.dump_stats('run_all_dev.profile')
+                                    profiler.enable()
 
 
 if __name__ == "__main__":
     run_all_experiments(run=0, dev=False, profiler=None)
+    # import cProfile
+    # pr = cProfile.Profile()
+    # pr.enable()
+    # run_all_experiments(run=1, dev=True, profiler=pr)
+    # pr.disable()
+    # pr.dump_stats('run_all_dev.profile')
+
+
 
 
