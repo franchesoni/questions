f833005d1c2cdb012932f3ae55bd87125235d14b
franchesoni <marchesoniacland@gmail.com>
2023-05-16 16:22:27 +0200

    config exps

--- main_ia.py
+++ main_ia.py
@@ -10,6 +10,7 @@ from intelligent_annotation import get_ia_curve
 
 def run_ia_experiment(
     sts_kwargs,
+    update_predictor_every,
     exp_type,
     experiment_name,
     dataset_index,
@@ -22,8 +23,8 @@ def run_ia_experiment(
     device=None,
 ):
     Path('results/curves').mkdir(parents=True, exist_ok=True)
-    assert exp_type == 'ia'
-    assert experiment_name in ['random', 'uncertainty']
+    # assert exp_type == 'ia'
+    # assert experiment_name in ['random', 'uncertainty']
     dataset_name = DATASET_NAMES[dataset_index]
     train_ds, test_ds = get_dataset(
         dataset_name,
@@ -46,6 +47,7 @@ def run_ia_experiment(
         curve = get_ia_curve(
             max_queries,
             predictor,
+            update_predictor_every,
             train_ds,
             test_ds,
             initial_labeled_indices=initial_labeled_indices,
@@ -57,40 +59,43 @@ def run_ia_experiment(
 def run_all_experiments(run=0, dev=False, profiler=None):
     for binarizer in ["geq5"]:
         for pretrained in [False]:#, True]:  # not working because it needs 1000 classes
-            for dataset_index in [1, 0, 2, 3]:#, 4]:
+            for max_n in [8, 16]:
                 for exp_name in ["uncertainty", "random"]:
-                    for max_n in [8, 4, 2, 16]:
+                    for dataset_index in [1, 0, 2, 3]:#, 4]:
                         for cost_fn in ["entropy", "length"]:
-                            for reduce_uncertainty_factor in [0.01, 0.5, 0.9, 0.1]:
+                            for reduce_uncertainty_factor in [0.01]:#, 0.5, 0.9, 0.1]:
+                                update_predictor_every = 10
                                 sts_kwargs = {
-                                    "max_expansions": 32,
+                                    "max_expansions": 8,
                                     "max_n": max_n,
                                     "al_method": exp_name,
                                     "cost_fn": cost_fn,
                                     "reduce_certainty_factor":reduce_uncertainty_factor,
+                                    "reset_tree":update_predictor_every==1,
                                 }
                                 exp_type = 'ia'
-                                max_queries = 1000
+                                max_queries = 2500
                                 use_only_first = 6000
                                 use_only_first_test = 1000
                                 if run == 0:
-                                    seeds = [0, 1, 2]
+                                    seeds = [0]#, 1, 2]
                                     device='cuda:0'
                                 elif run == 1:
-                                    seeds = [3, 4, 5]
+                                    seeds = [1]#, 4, 5]
                                     device='cuda:1'
                                     # device='cpu'
                                 else:
                                     raise ValueError("run must be 0 or 1")
 
                                 if dev:
-                                    max_queries = 10
+                                    max_queries = 30
                                     use_only_first = 6000
                                     use_only_first_test = 1000
                                     seeds = [0]
 
                                 run_ia_experiment(
                                     sts_kwargs=sts_kwargs,
+                                    update_predictor_every=update_predictor_every,
                                     seeds=seeds,
                                     exp_type=exp_type,
                                     experiment_name=exp_name,
@@ -113,11 +118,11 @@ def run_all_experiments(run=0, dev=False, profiler=None):
 
 
 if __name__ == "__main__":
-    run_all_experiments(run=0, dev=False, profiler=None)
+    run_all_experiments(run=1, dev=False, profiler=None)
     # import cProfile
     # pr = cProfile.Profile()
     # pr.enable()
-    # run_all_experiments(run=1, dev=True, profiler=pr)
+    # run_all_experiments(run=0, dev=True, profiler=pr)
     # pr.disable()
     # pr.dump_stats('run_all_dev.profile')
 
