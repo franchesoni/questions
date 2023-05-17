fb74f020541e20b5a663c117b31452792858a413
franchesoni <marchesoniacland@gmail.com>
2023-05-17 08:15:50 +0200

    update

--- simulations.py
+++ simulations.py
@@ -101,13 +101,13 @@ def extend_results(results, name, value):
 
 
 ########### functions to run methods ############
-def run_STS(predictions, labels, results, cost_fn="length"):
+def run_STS(predictions, labels, results, cost_fn="entropy"):
     N = len(labels)
     # simple tree search
     alia = STS(
-        max_expansions=10, al_method="uncertainty", max_n=10, cost_fn=cost_fn
+        max_expansions=8, al_method="uncertainty", max_n=8, cost_fn=cost_fn, reduce_certainty_factor=0.01, reset_tree=False
     )
-    state = {"indices": list(range(N)), "incorrect": None}
+    state = {"indices": np.arange(N), "incorrect": None}
     root_node = STS.initialize_root_node(state)
 
     ### query ###
@@ -118,36 +118,30 @@ def run_STS(predictions, labels, results, cost_fn="length"):
         n_questions += 1
         new_predictions = predictions[state["indices"]]
         alia.set_unlabeled_predictions(
-            (state["indices"], new_predictions.tolist())
+            (state["indices"], new_predictions)
         )
         best_question_node = alia.tree_search(root_node)
         question, optimal_cost, correct_prob = best_question_node.guess, best_question_node.cost, best_question_node.children_probs[1]
-        entropy1 = entropy_given_state_preds_binary(root_node.state, (state["indices"], new_predictions.tolist()))
+        entropy1 = entropy_given_state_preds_binary(root_node.state, (state["indices"], new_predictions))
         entropy2 = entropy_given_probs_binary(new_predictions)
-        if optimal_cost > entropy1 + 1:
-            condition = n_questions == 1 or ((root_node.state['incorrect'] is not None) and (prev == 'above'))
-            print("above", condition)
-            prev = 'above'
-        else:
-            condition = n_questions == 1 or ((root_node.state['incorrect'] is not None) and (prev == 'above'))
-            print("below", condition)
-            prev = 'below'
+        # if optimal_cost > entropy1 + 1:
+        #     condition = n_questions == 1 or ((root_node.state['incorrect'] is not None) and (prev == 'above'))
+        #     print("above", condition)
+        #     prev = 'above'
+        # else:
+        #     condition = n_questions == 1 or ((root_node.state['incorrect'] is not None) and (prev == 'above'))
+        #     print("below", condition)
+        #     prev = 'below'
         estimated_costs.append((optimal_cost, entropy1, entropy2, n_questions))
         assert 0 < len(question[0]), "you should ask something"
-        answer = all(
-            [
-                guessed_label == labels[index]
-                for index, guessed_label in zip(*question)
-            ]
-        )
+        answer = bool((question[1] == labels[question[0]]).all())
         probs_and_outcomes.append((correct_prob, answer))
         state = STS.update_state(root_node.state, question, answer)
-        root_node = STS.set_new_root_node(best_question_node.state_children[answer*1])
-        assert root_node.state == state
+        root_node = STS.set_new_root_node(best_question_node.state_children[answer*1], destroy_descendants=False)
     extend_results(results, "probs_and_outcomes_sts", probs_and_outcomes)
     extend_results(results, "estimated_costs_sts", estimated_costs)
     extend_results(results, "n_questions_sts", n_questions)
-    print('-'*20)
+    # print('-'*20)
     return results
 
 
@@ -182,7 +176,7 @@ def run_huffman(predictions, labels, results):
 
 
 def main(n_seeds, N, generator_fn, dstdir):
-    reset = True
+    reset = False
     Path(dstdir).mkdir(parents=True, exist_ok=True)
     dstfilename = f"{n_seeds}_{N}_{generator_fn.__name__}"
     print(f"Running {dstfilename}")
@@ -193,8 +187,8 @@ def main(n_seeds, N, generator_fn, dstdir):
     # custom
     results = {}
     if not os.path.exists(records_filename) or reset:
-        # for seed in tqdm.tqdm(range(n_seeds)):
-        for seed in range(n_seeds):
+        for seed in tqdm.tqdm(range(n_seeds)):
+        # for seed in range(n_seeds):
             np.random.seed(seed)
             (
                 pos_samples,
@@ -229,7 +223,7 @@ def main(n_seeds, N, generator_fn, dstdir):
 
 
 if __name__ == "__main__":
-    n_seeds, N, dstdir = 100, 10, "results/simulations"
+    n_seeds, N, dstdir = 1000, 10, "results/simulations"
     Path(dstdir).mkdir(parents=True, exist_ok=True)
     dstfilename = f"{n_seeds}_{N}"
     # import cProfile
@@ -239,6 +233,39 @@ if __name__ == "__main__":
     # cProfile.run(
     #     command, sort="cumtime", filename=f"{dstdir}/{dstfilename}_simulation.profile"
     # )
-    for generator_fn in [generate_2d_gaussians_and_predictor]:#, generate_2d_gaussians_with_linear_and_predictor, generate_2d_classification_and_predictor]:
+    np.random.seed(42)
+    for generator_fn in [
+        generate_2d_gaussians_and_predictor,
+        generate_2d_gaussians_with_linear_and_predictor,
+        generate_2d_classification_and_predictor]:
+        # (
+        #     pos_samples,
+        #     neg_samples,
+        #     prob_of_being_positive,
+        # ) = generator_fn(N)
+        # X = np.concatenate((neg_samples, pos_samples), axis=0)
+        # y = np.array([0] * len(neg_samples) + [1] * len(pos_samples))
+        # X_to_annotate = X
+        # y_to_annotate = y
+        # indices_to_ask = []
+        # predictions = []
+        # X_annotated = np.empty((0, 2))
+        # y_annotated = np.empty((0, 1))
+
+        # vis.plot_model_outputs(
+        #     prob_of_being_positive,
+        #     None,
+        #     X,
+        #     y,
+        #     X_to_annotate,
+        #     y_to_annotate,
+        #     indices_to_ask,
+        #     predictions,
+        #     X_annotated,
+        #     y_annotated,
+        #     generator_fn.__name__,
+        # )
+
+
         main(n_seeds, N, generator_fn, dstdir)
 
